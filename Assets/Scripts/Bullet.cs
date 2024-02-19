d5b739a9334462a4bf7073fc35afea24d52a3e36
Kevin Cerro DVRV <kevincerro.dvrv@gmail.com>
2024-02-19 20:25:17 +0100

    Implement cannon recoil

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
     public GameObject explosionPrefab;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start() {
-        GetComponent<Rigidbody>().AddForce(transform.forward * 600f, ForceMode.Impulse);
-        
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 600f, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.x > 0.3 || rb.velocity.y > 0.3 || rb.velocity.z > 0.3) {
+            transform.LookAt(transform.position + rb.velocity);
+        }
     }
 
     void OnCollisionEnter(Collision other) {
--- Assets/Scripts/CannonRecoil.cs
+++ Assets/Scripts/CannonRecoil.cs
@@ -7,13 +7,18 @@ public class CannonRecoil : MonoBehaviour {
     public float recoilSpeed;
     [Tooltip("Acceleration to get the cannon back to the starting position")]
     public float restorationAcceleration;
+    [Tooltip("Max speed archievable when returning to the original position")]
+    public float maxRestorationSpeed;
+    private float speed;
 
     private bool recoilActive;
+    private Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         recoilActive = false;
+        startPosition = transform.localPosition;
     }
 
     // Update is called once per frame
@@ -23,9 +28,19 @@ public class CannonRecoil : MonoBehaviour {
             return;
         }
         
+        transform.position += speed * Time.deltaTime * transform.forward;
+        speed += restorationAcceleration * Time.deltaTime;
+        speed = Mathf.Clamp(speed, -recoilSpeed, maxRestorationSpeed);
+
+        if (transform.localPosition.z > startPosition.z) {
+            speed = 0;
+            recoilActive = false;
+            transform.localPosition = startPosition;
+        }
     }
 
     public void StartRecoil() {
         recoilActive = true;
+        speed = -recoilSpeed;
     }
 }
